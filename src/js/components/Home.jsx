c112e43c2494414110d2bea9633079de88abddfe
marcostorresf <marcos_e19@icloud.com>
2025-03-21 23:29:02 +0000

    Done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,35 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-    const [InputValue, setInputValue] = useState ("")
-	const [ToDo, setToDo] = useState ([])
-    return (
+	const [InputValue, setInputValue] = useState("")
+	const [ToDo, setToDo] = useState([])
+	return (
 		<div className="container">
-			 <h1>To Do List</h1>
-			 <ul>
-				<li><input 
-				           type ="text"
-						   onChange={(e) => setInputValue(e.target.value)}
-						   value={InputValue}
-						   onKeyPress={(e) => {
-							 if (e.key === "Enter") {
-                                setToDo(ToDo.concat(InputValue));
-								setInputValue("");
-							}
-						   }}
-				           placeholder="What do you need to do"></input> 
+			<h1>To Do List</h1>
+			<ul>
+				<li><input
+					type="text"
+					onChange={(e) => setInputValue(e.target.value)}
+					value={InputValue}
+					onKeyPress={(e) => {
+						if (e.key === "Enter") {
+							setToDo(ToDo.concat(InputValue));
+							setInputValue("");
+						}
+					}}
+					placeholder="What do you need to do?"></input>
 				</li>
-				{ToDo.map((t) => (
-				   <li>
-					 {t} <i class="fa-solid fa-xmark"></i>
-				   </li>
-				  
-				
-			))}
-			
-			 </ul>
-			 <div>15 Tasks</div>
+				{ToDo.map((item, index) => (
+					<li>
+						{item}{" "} <span className="" onClick={() => setToDo(ToDo.filter((t, currentIndex) => index != currentIndex))}>X</span>
+					</li>
+
+
+				))}
+
+			</ul>
+			<div className="item">{ToDo.length} item left </div>
 
 
 		</div>

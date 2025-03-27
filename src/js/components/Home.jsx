e3f74a2dd113ae6784d230ba8b9d4fa95fc58563
marcostorresf <marcos_e19@icloud.com>
2025-03-27 00:57:20 +0000

    doing my best

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,9 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
 	const [InputValue, setInputValue] = useState("")
 	const [ToDo, setToDo] = useState([])
+
+	useEffect(()=> {
+
+		getUser()
+
+	},[])
+
+	const getUser = async() => {
+		let response = await fetch('https://playground.4geeks.com/todo/users/marcostorresf')
+        let data = await response.json()
+		console.log(data)
+		if(typeof data.name !='undefined') {
+			setToDo(data.todos)
+			console.log(data.name)
+		}
+		else if(typeof data.name !='undefined'){
+			let response = await fetch('https://playground.4geeks.com/todo/users/marcostorresf',{
+				method: "POST",
+                headers: { "Content-type": "application/json" },
+    
+			})
+			let data = await response.json()
+			// console.log(data)
+			
+		}
+
+	}
 	return (
 		<div className="container">
 			<h1>To Do List</h1>
@@ -22,7 +49,7 @@ const Home = () => {
 				</li>
 				{ToDo.map((item, index) => (
 					<li>
-						{item}{" "} <span className="" onClick={() => setToDo(ToDo.filter((t, currentIndex) => index != currentIndex))}>X</span>
+						{item.label}{" "} <span className="" onClick={() => setToDo(ToDo.filter((t, currentIndex) => index != currentIndex))}>X</span>
 					</li>
 
 
